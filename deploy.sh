9a57aed1da38de526a382d6f8610c58e6aa6d704
Marcelo Canina <me@marcanuy.com>
2017-05-16 00:38:13 -0300

    added html minifier to deploy commented

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,14 @@ set -e # halt script on error
 set -v # echo on
 
 echo "Pushing to periscopio"
-#git push periscopio
+git push periscopio
 
 echo "Building site"
 JEKYLL_ENV=production bundle exec jekyll build
+
+#html-minifier --collapse-whitespace --trim-custom-fragments --remove-comments --html5 --minify-css --minify-js --input-dir _site --output-dir _site --file-ext html
+#html-minifier --remove-comments --minify-css --input-dir _site --output-dir _site --file-ext css
+#html-minifier --minify-js --input-dir _site --output-dir _site --file-ext js
 
 # remove .html extension
 find _site/ -type f ! -iname 'index.html' -iname '*.html' -print0 | while read -d $'\0' f; do mv "$f" "${f%.html}"; done
